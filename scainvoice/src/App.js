b3b480d1d481f27c6c8a83a1bc827f18782d3dbb
kanueven <user@DESKTOP-993RSPE.tenable.sca.spartec.co.ke>
2023-06-22 13:14:32 +0300

    added sidebar

--- scainvoice/src/App.js
+++ scainvoice/src/App.js
@@ -1,10 +1,12 @@
 
 import './App.css';
 import Topbar from "./components/Topbar"
+import Sidebar from "./components/sidebar/Sidebar"
 function App() {
   return (
     <>
     <Topbar/>
+    <Sidebar/>
     </>
   );
 }
---
+++ scainvoice/src/components/pages/Invoice/DispatchedInvoice.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function DispatchedInvoice() {
+  return (
+    <div>DispatchedInvoice</div>
+  )
+}
