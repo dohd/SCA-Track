ab04bd49654d73a6ade2badb364da16f0ae8b8cd
fourthperson <iankariithi@gmail.com>
2023-06-27 22:55:54 +0300

    start basic routing implementation

--- scainvoice/src/App.js
+++ scainvoice/src/App.js
@@ -1,36 +1,33 @@
-
 import './App.css';
 import Topbar from "./components/Topbar"
-import Home from "./components/pages/home/Home"
-
-import Invoice from "./components/pages/Invoice/InvoiceList"
 import Sidebar from "./components/sidebar/Sidebar"
-import Customer  from './components/pages/customer/Customer';
-import {BrowserRouter as Router,Route,Routes,} from "react-router-dom";
-import CreateNewCustomer from './components/pages/customer/CreateNewCustomer';
-import InvoiceList from './components/pages/Invoice/InvoiceList';
+import {BrowserRouter as Router, Route, Routes,} from "react-router-dom";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
+import React from "react";
+
 function App() {
-  return (
-   
-    <Router>
-    <Topbar />
-    <div className="container">
-      <Sidebar />
-      <Routes>
-        <Route  path="/" component={Home} />
-        <Route path="/Invoice" component={Invoice} />
-        <Route exact path="/InvoiceList" component={InvoiceList} />
-        <Route path="/Customer" component={Customer} />
+    return (
 
-        <Route path="/CreateNewCustomer" component={CreateNewCustomer} />
+        <Router>
+            <Topbar/>
+            <div className="container">
+                <Sidebar/>
+                {/*<Routes>*/}
+                {/*    <Route path="/" component={Home}/>*/}
+                {/*    <Route path="/Invoice" component={Invoice}/>*/}
+                {/*    <Route exact path="/InvoiceList" component={InvoiceList}/>*/}
+                {/*    <Route path="/Customer" component={Customer}/>*/}
 
-      </Routes>
-    </div>
-  </Router>
-    
-      
-   
-  );
+                {/*    <Route path="/CreateNewCustomer" component={CreateNewCustomer}/>*/}
+
+                {/*</Routes>*/}
+            </div>
+        </Router>
+
+
+    );
 }
+
+// <ArrowBackIcon style={{transform: open ? 'rotate(0deg)' : 'rotate(180deg)'}}/>
 
 export default App;
--- scainvoice/src/components/sidebar/Sidebar.jsx
+++ scainvoice/src/components/sidebar/Sidebar.jsx
@@ -1,69 +1,61 @@
 import React from 'react'
-import { styled, createTheme, ThemeProvider } from '@mui/material/styles';
+import {styled, createTheme, ThemeProvider} from '@mui/material/styles';
 import Box from '@mui/material/Box'
 import MuiAppBar from '@mui/material/AppBar';
 import CssBaseline from '@mui/material/CssBaseline'
 import Toolbar from '@mui/material/Toolbar';
 import IconButton from '@mui/material/IconButton';
-import { Link } from 'react-router-dom';
-import Dispatched from './Dispatched'
-import Customerdb from './Customerdb'
-import Lpodb from './Lpodb';
 import MuiDrawer from '@mui/material/Drawer';
-import Grid from '@mui/material/Grid'
-import Paper from '@mui/material/Paper';
-
 import Container from '@mui/material/Container'
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 import List from '@mui/material/List';
 import Divider from '@mui/material/Divider';
-import { mainListItems} from './ListItems';
-import Distributor from './Distributorsdb';
-import { green } from '@mui/material/colors';
+import {mainListItems} from './ListItems';
+import {green} from '@mui/material/colors';
 
 const drawerWidth = 240;
 const AppBar = styled(MuiAppBar, {
-  shouldForwardProp: (prop) => prop !== 'open',
-})(({ theme, open }) => ({
-  zIndex: theme.zIndex.drawer + 1,
-  transition: theme.transitions.create(['width', 'margin'], {
-    easing: theme.transitions.easing.sharp,
-    duration: theme.transitions.duration.leavingScreen,
-  }),
-  ...(open && {
-    marginLeft: drawerWidth,
-    width: `calc(100% - ${drawerWidth}px)`,
+    shouldForwardProp: (prop) => prop !== 'open',
+})(({theme, open}) => ({
+    zIndex: theme.zIndex.drawer + 1,
     transition: theme.transitions.create(['width', 'margin'], {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.enteringScreen,
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.leavingScreen,
     }),
-  }),
+    ...(open && {
+        marginLeft: drawerWidth,
+        width: `calc(100% - ${drawerWidth}px)`,
+        transition: theme.transitions.create(['width', 'margin'], {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.enteringScreen,
+        }),
+    }),
 }));
 
-const Drawer = styled(MuiDrawer, { shouldForwardProp: (prop) => prop !== 'open' })(
-  ({ theme, open }) => ({
-    '& .MuiDrawer-paper': {
-      position: 'relative',
-      whiteSpace: 'nowrap',
-      width: drawerWidth,
-      transition: theme.transitions.create('width', {
-        easing: theme.transitions.easing.sharp,
-        duration: theme.transitions.duration.enteringScreen,
-      }),
-      boxSizing: 'border-box',
-      ...(!open && {
-        overflowX: 'hidden',
-        transition: theme.transitions.create('width', {
-          easing: theme.transitions.easing.sharp,
-          duration: theme.transitions.duration.leavingScreen,
-        }),
-        width: theme.spacing(7),
-        [theme.breakpoints.up('sm')]: {
-          width: theme.spacing(9),
+const Drawer = styled(MuiDrawer, {shouldForwardProp: (prop) => prop !== 'open'})(
+    ({theme, open}) => ({
+        '& .MuiDrawer-paper': {
+            position: 'relative',
+            whiteSpace: 'nowrap',
+            width: drawerWidth,
+            transition: theme.transitions.create('width', {
+                easing: theme.transitions.easing.sharp,
+                duration: theme.transitions.duration.enteringScreen,
+            }),
+            boxSizing: 'border-box',
+            ...(!open && {
+                overflowX: 'hidden',
+                transition: theme.transitions.create('width', {
+                    easing: theme.transitions.easing.sharp,
+                    duration: theme.transitions.duration.leavingScreen,
+                }),
+                width: theme.spacing(7),
+                [theme.breakpoints.up('sm')]: {
+                    width: theme.spacing(9),
+                },
+            }),
         },
-      }),
-    },
-    backgroundColor:green
+        backgroundColor: green
     }),
 );
 
@@ -71,111 +63,53 @@ const Drawer = styled(MuiDrawer, { shouldForwardProp: (prop) => prop !== 'open' 
 const defaultTheme = createTheme();
 
 export default function Sidebar() {
-  const [open, setOpen] = React.useState(true);
-  const toggleDrawer = () => {
-    setOpen(!open);
-  };
+    const [open, setOpen] = React.useState(true);
+    const toggleDrawer = () => {
+        setOpen(!open);
+    };
 
-  return (
-    <ThemeProvider theme={defaultTheme}>
-    <Box sx={{ display: 'flex' }}>
-    <CssBaseline />
-    <AppBar position="absolute" open={open}>
-      
-          
-    </AppBar>
-    <Drawer variant="permanent" open={open}>
-      <Toolbar
-        sx={{
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'flex-end',
-          px: [1],
-        }}
-      >
-        <IconButton onClick={toggleDrawer}>
-          <ArrowBackIcon />
-        </IconButton>
-      </Toolbar>
-      <Divider />
-      <List component="nav">
-        {mainListItems}
-        
-      </List>
-    </Drawer>
-    <Box
-      component="main"
-      sx={{
-        backgroundColor: (theme) =>
-          theme.palette.mode === 'light'
-            ? theme.palette.grey[100]
-            : theme.palette.grey[900],
-        flexGrow: 1,
-        height: '100vh',
-        overflow: 'auto',
-      }}
-    >
-      <Toolbar />
-      <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
-        <Grid container spacing={3}>
-           {/* Dispatched Invoice */}
-           <Grid item xs={12} md={4} lg={3}>
-                <Paper
-                  sx={{
-                    p: 2,
-                    display: 'flex',
-                    flexDirection: 'column',
-                    height: 240,
-                  }}
-                >
-                  <Dispatched />
-                </Paper>
-              </Grid>
-          {/* Recent Customers */}
-          <Grid item xs={12} md={4} lg={3}>
-            <Paper
-              sx={{
-                p: 2,
-                display: 'flex',
-                flexDirection: 'column',
-                height: 240,
-              }}
-            >
-              <Customerdb />
-            </Paper>
-          </Grid>
-          {/* LPO */}
-          <Grid item xs={12} md={4} lg={3}>
-                <Paper
-                  sx={{
-                    p: 2,
-                    display: 'flex',
-                    flexDirection: 'column',
-                    height: 240,
-                  }}
-                >
-                  <Lpodb />
-                </Paper>
-              </Grid>
-              
-              <Grid item xs={12} md={4} lg={3}>
-                <Paper
-                  sx={{
-                    p: 2,
-                    display: 'flex',
-                    flexDirection: 'column',
-                    height: 240,
-                  }}
-                >
-                  <Distributor />
-                </Paper>
-              </Grid>
-        </Grid>
-        
-      </Container>
-    </Box>
-  </Box>
-</ThemeProvider>
+    return (
+        <ThemeProvider theme={defaultTheme}>
+            <Box sx={{display: 'flex'}}>
+                <CssBaseline/>
+                <AppBar position="absolute" open={open}>
 
-  )
+                </AppBar>
+                <Drawer variant="permanent" open={open}>
+                    <Toolbar
+                        sx={{
+                            display: 'flex',
+                            alignItems: 'center',
+                            justifyContent: 'flex-end',
+                            px: [1],
+                        }}
+                    >
+                        <IconButton onClick={toggleDrawer}>
+                            {/* rotate the arrow icon if open or closed */}
+                            <ArrowBackIcon style={{transform: open ? 'rotate(0deg)' : 'rotate(180deg)'}}/>
+                        </IconButton>
+                    </Toolbar>
+                    <Divider/>
+                    <List component="nav">
+                        {mainListItems}
+                    </List>
+                </Drawer>
+                <Box
+                    component="main"
+                    sx={{
+                        backgroundColor: (theme) =>
+                            theme.palette.mode === 'light'
+                                ? theme.palette.grey[100]
+                                : theme.palette.grey[900],
+                        flexGrow: 1,
+                        height: '100vh',
+                        overflow: 'auto',
+                    }}
+                >
+
+                </Box>
+            </Box>
+        </ThemeProvider>
+
+    )
 }
