24e42cb072a61337782c4094c4861c055b3c5db1
Kanueven <raelrakeli@gmail.com>
2023-07-03 02:20:55 +0300

    dash

---
+++ scainvoice/src/pages/dashboard/Dashboard.jsx
@@ -0,0 +1,99 @@
+import * as React from 'react';
+import { createTheme, styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+
+import MuiAppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+
+import CssBaseline from '@mui/material/CssBaseline';
+import Typography from '@mui/material/Typography';
+import { useState,useMemo } from 'react';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import Sidelist from './Sidelist';
+import { ThemeProvider } from '@emotion/react';
+
+
+const drawerWidth = 240;
+
+
+
+
+const AppBar = styled(MuiAppBar, {
+  shouldForwardProp: (prop) => prop !== 'open',
+})(({ theme, open }) => ({
+  zIndex: theme.zIndex.drawer + 1,
+  transition: theme.transitions.create(['width', 'margin'], {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  ...(open && {
+    marginLeft: drawerWidth,
+    width: `calc(100% - ${drawerWidth}px)`,
+    transition: theme.transitions.create(['width', 'margin'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  }),
+}));
+
+
+
+export default function Dashboard() {
+  
+  const [open, setOpen] = useState(false);
+  const [dark, setDark] = useState(true);
+  
+{/*In the code above, we define a state variable dark using the useState hook, and initialize it with the value true. We also have a setDark function to update the value of dark when needed.
+
+The darkTheme variable is created using the useMemo hook to memoize the theme object. This means the theme will only be recreated if the value of dark changes.
+
+Inside createTheme, we define the palette object with a mode property. The value of mode is set based on the dark state. If dark is true, the mode will be 'dark'; otherwise, it will be 'light'. */}
+  const greenTheme = useMemo(() =>
+  createTheme({
+    palette: {
+      mode: dark ? 'dark' : 'light',
+      primary: {
+        main: '#00ff00', 
+          },
+    },
+  }), [dark]);
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  
+  return (
+    <ThemeProvider theme={greenTheme}>
+    <Box sx={{ display: 'flex' }}>
+      <CssBaseline />
+      <AppBar position="fixed" open={open} style={{backgroundColor:'green'}}>
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            onClick={handleDrawerOpen}
+            edge="start"
+            sx={{
+              marginRight: 5,
+              ...(open && { display: 'none' }),
+            }}
+          >
+            <MenuIcon />
+          </IconButton>
+
+          <Typography variant="h6" noWrap component="div" sx={{flexGrow:1}}>
+            Spartec   
+         </Typography>
+         <IconButton>
+          {/*color mode here*/}
+         </IconButton>
+        </Toolbar>
+      </AppBar>
+      <Sidelist {...{open,setOpen}}/>
+      </Box>
+      </ThemeProvider>
+      
+  );
+}
---
+++ scainvoice/src/pages/dashboard/Sidelist.jsx
@@ -0,0 +1,152 @@
+import React, { useMemo, useState } from 'react'
+import { styled } from '@mui/material/styles';
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import InboxIcon from '@mui/icons-material/MoveToInbox';
+import Box from '@mui/material/Box';
+import {dispatch} from 'react'
+import { Route, useNavigate ,Routes} from 'react-router-dom';
+import Distributors from '../../components/distributors/Distributors'
+import { IconButton, Tooltip } from '@mui/material';
+import MailIcon from '@mui/icons-material/Mail';
+import Divider from '@mui/material/Divider';
+import Typography from '@mui/material/Typography';
+import List from '@mui/material/List';
+import MuiDrawer from '@mui/material/Drawer';
+import Home from '../../components/home/Home';
+import Logout from '@mui/icons-material/Logout'
+
+
+import distributors from '../../components/distributors/Distributors';
+const drawerWidth = 240;
+
+
+const openedMixin = (theme) => ({
+    width: drawerWidth,
+    transition: theme.transitions.create('width', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+    overflowX: 'hidden',
+  });
+  
+  const closedMixin = (theme) => ({
+    transition: theme.transitions.create('width', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    overflowX: 'hidden',
+    width: `calc(${theme.spacing(7)} + 1px)`,
+    [theme.breakpoints.up('sm')]: {
+      width: `calc(${theme.spacing(8)} + 1px)`,
+    },
+  });
+const DrawerHeader = styled('div')(({ theme }) => ({
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+    padding: theme.spacing(0, 1),
+    // necessary for content to be below app bar
+    ...theme.mixins.toolbar,
+  }));
+  const Drawer = styled(MuiDrawer, { shouldForwardProp: (prop) => prop !== 'open' })(
+    ({ theme, open }) => ({
+      width: drawerWidth,
+      flexShrink: 0,
+      whiteSpace: 'nowrap',
+      boxSizing: 'border-box',
+      ...(open && {
+        ...openedMixin(theme),
+        '& .MuiDrawer-paper': openedMixin(theme),
+      }),
+      ...(!open && {
+        ...closedMixin(theme),
+        '& .MuiDrawer-paper': closedMixin(theme),
+      }),
+    }),
+  );
+const Sidelist=({open,setOpen})=> {
+  const navigate =useNavigate()
+  const handlelogout= () =>{
+  dispatch({type:'UPDATE_USER',payload:null})
+  navigate('/')
+  }
+
+  const {selectedLink,setselectedLink}=useState('')
+  const list = useMemo(()=>[
+    {title:'Home',icon:<MailIcon/>,link:'',component:<Home{...{setselectedLink,link:''}}/>},
+    {title:'Distributors',icon:<InboxIcon/>,link:'distributors',component:< 
+      Distributors {...{setselectedLink,link:'distributors'}}/>},
+  ],[])
+
+  return (
+   <>
+   
+   <Drawer variant="permanent" open={open}>
+        <DrawerHeader>
+          <IconButton onClick={()=>setOpen(false)}>
+            <ChevronLeftIcon />
+          </IconButton>
+        </DrawerHeader>
+        <Divider />
+        <List>
+          {list.map((item) => (
+            <ListItem key={item.title} disablePadding sx={{ display: 'block' }}>
+              <ListItemButton
+                sx={{
+                  minHeight: 48,
+                  justifyContent: open ? 'initial' : 'center',
+                  px: 2.5,
+                }}
+                onClick={()=>navigate(item.link)}
+                selected ={selectedLink=== item,link}
+              >
+                <ListItemIcon
+                  sx={{
+                    minWidth: 0,
+                    mr: open ? 3 : 'auto',
+                    justifyContent: 'center',
+                  }}
+                >
+                  {item.icon}
+                </ListItemIcon>
+                <ListItemText primary={item.title} sx={{ opacity: open ? 1 : 0 }} />
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </List>
+        <Divider />
+       <Tooltip title= 'Logout' sx={{mt:1}}>
+        <IconButton onClick={handlelogout}> 
+          <Logout/>
+        </IconButton>
+
+       </Tooltip>
+      </Drawer>
+      <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+        <DrawerHeader />
+        <Routes>
+          {list.map(item=>(
+            <Route key={item.title} path={item.link} element={item.component}/>
+          ))}
+        </Routes>
+        <Typography >
+        <Home/>
+        </Typography>
+        <Typography >
+            <Home/>
+         
+        </Typography>
+      </Box>
+   
+   
+   
+   
+   </>
+  )
+}
+
+export default Sidelist
